f0adf20f24aec0e47e10a915795d4e728c94bf6d
Evgenij Vasilev <vasil.ev.genij@gmail.com>
2016-08-08 14:40:10 +0300

    Add provider module

---
+++ provider/src/main/java/ru/yandex/yamblz/euv/provider/DebugActivity.java
@@ -0,0 +1,12 @@
+package ru.yandex.yamblz.euv.provider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
